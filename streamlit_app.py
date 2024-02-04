0a0908d78aa20d3d11e1026e14a7910c906ef444
stephotee <143778270+stephotee@users.noreply.github.com>
2024-02-04 14:21:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,106 +1,75 @@
 import streamlit as st
-from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import pandas as pd
-from io import StringIO
+from wordcloud import WordCloud, STOPWORDS
 import nltk
+from nltk.tokenize import word_tokenize
+import io
+import base64
+import pandas as pd
 
-nltk.download('stopwords')
+# Initialize NLTK resources
 nltk.download('punkt')
 
-# Initialize NLTK stop words
-nltk_stopwords = stopwords.words('english')
+# Function to generate word cloud
+def generate_word_cloud(text, max_words, color_scheme, text_case, additional_stop_words):
+    # Tokenization
+    tokens = word_tokenize(text)
 
-# Function to generate the word cloud
-def generate_wordcloud(text_data, additional_stopwords, max_words, color_scheme, text_case):
-    # Tokenize words
-    tokens = word_tokenize(text_data)
-    # Remove punctuation and make lower case
-    tokens = [word.lower() for word in tokens if word.isalpha()]
+    # Stop words removal
+    stop_words = set(STOPWORDS).union(set(additional_stop_words))
+    tokens = [word for word in tokens if word.lower() not in stop_words]
     
-    # Apply text case
+    # Text case conversion
     if text_case == 'Upper case':
         tokens = [word.upper() for word in tokens]
     elif text_case == 'Lower case':
         tokens = [word.lower() for word in tokens]
 
-    # Add additional stop words if any
-    if additional_stopwords:
-        additional_stopwords = [word.strip().lower() for word in additional_stopwords.split(',')]
-        all_stopwords = nltk_stopwords + additional_stopwords
-    else:
-        all_stopwords = nltk_stopwords
-
-    # Remove stop words
-    tokens = [word for word in tokens if not word in all_stopwords]
-
     # Generate word cloud
     wordcloud = WordCloud(
-        width=800, 
+        width=800,
         height=400,
-        max_font_size=60, 
-        min_font_size=8, 
-        max_words=max_words, 
-        background_color='white',
-        color_func=lambda *args, **kwargs: color_scheme
+        max_words=max_words,
+        color_func=(lambda *args, **kwargs: color_scheme) if color_scheme != 'black' else None,
+        background_color='white'
     ).generate(' '.join(tokens))
 
-    # Display the word cloud
+    # Display the generated image:
     plt.figure(figsize=(10, 5))
     plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    st.pyplot()
+    plt.axis("off")
+    plt.show()
 
-# Function to handle color scheme
-def get_color_scheme(text_colour):
-    if text_colour == 'Black text':
-        return 'black'
-    elif text_colour == 'Colourful':
-        return lambda *args, **kwargs: "hsl(%d, 100%%, 50%%)" % np.random.randint(0, 360)
-
-# Streamlit UI layout
+# Streamlit app layout
 st.title('Word Cloud Generator')
 
-# Sidebar controls
-st.sidebar.title("Controls")
-number_of_words = st.sidebar.slider('Number of words', 5, 100, 5, 5)
-text_colour = st.sidebar.selectbox('Text colour', ['Black text', 'Colourful'])
-text_case = st.sidebar.selectbox('Text case', ['Upper case', 'Lower case'])
-additional_stop_words = st.sidebar.text_area('Additional stop words', '')
-custom_groupings = {}
+# Text input
+text_input = st.text_area("Paste text here (use commas to separate words or phrases)")
 
-# Upload file
-uploaded_file = st.file_uploader("Choose a text or CSV file", type=['txt', 'csv'])
+# File upload
+uploaded_file = st.file_uploader("Or upload your data (.csv or .txt)", type=['csv', 'txt'])
+
+# Process uploaded file
 if uploaded_file is not None:
     if uploaded_file.type == "text/csv":
         df = pd.read_csv(uploaded_file)
-        text_data = ' '.join(df[df.columns[0]].dropna().astype(str).tolist())
-    else:
-        text_data = uploaded_file.getvalue().decode("utf-8")
+        text_input = ' '.join(df.iloc[:, 0].astype(str).tolist())
+    elif uploaded_file.type == "text/plain":
+        text_input = uploaded_file.read().decode('utf-8')
 
-# Text input
-else:
-    text_data = st.text_area("Paste text here...")
+# Side bar for additional controls
+max_words = st.sidebar.slider("Number of words", 5, 100, 50, 5)
+color_scheme = st.sidebar.selectbox("Text color", options=['black', 'Colorful'])
+text_case = st.sidebar.radio("Text case", ('Upper case', 'Lower case'))
+additional_stop_words = st.sidebar.text_input("Additional stop words", value='').split(',')
+
+# Custom groups (not fully implemented)
+# This part of the code needs to be implemented according to the specifics of the custom grouping functionality
 
 # Button to generate word cloud
 if st.button('Generate Word Cloud'):
-    color_scheme = get_color_scheme(text_colour)
-    generate_wordcloud(text_data, additional_stop_words, number_of_words, color_scheme, text_case)
+    color_scheme = 'black' if color_scheme == 'black' else plt.cm.rainbow
+    generate_word_cloud(text_input, max_words, color_scheme, text_case, additional_stop_words)
 
-# Button to start over
-if st.button('Start Over'):
-    st.experimental_rerun()
-
-# Save the current word cloud as a PNG
-if st.button('Download PNG'):
-    wordcloud.to_file('wordcloud.png')
-    with open('wordcloud.png', 'rb') as file:
-        btn = st.download_button(
-            label="Download PNG",
-            data=file,
-            file_name="wordcloud.png",
-            mime="image/png"
-        )
+# Download button (not fully implemented)
+# Here you need to implement the function to convert the generated word cloud to a PNG and download it
